58103822263f2fdee7cd2d0badc4325f84081b1d
Nick Kezhaya <nick@whitepaperclip.com>
2013-08-08 17:19:37 -0500

    Modularized the server code for singleton access

--- server.js
+++ server.js
@@ -1,18 +1,2 @@
-var dgram = require("dgram"),
-    server = dgram.createSocket("udp4"),
-    parser = require("./lib/parser");
-
-server.on("message", function (msg, rinfo) {
-    try {
-        parser.receipt(msg, rinfo);
-    } catch (e) {
-        console.log("Exception caught on message receipt.", e);
-    }
-});
-
-server.on("listening", function () {
-    var address = server.address();
-    console.log("Server listening on: " + address.address + ":" + address.port);
-});
-
+var server = require("./lib/server").server;
 server.bind(31337);
